15f0c1a9e076159ce0f0cc07dcc7604bf4da757d
Anshul Ojha <anshulojha091@gmail.com>
2025-07-08 10:21:03 +0530

    update socket connection URL to use production server

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import Editor from "@monaco-editor/react";
-const socket = io("http://localhost:3000");
+const socket = io("https://coderelay-server.onrender.com");
 
 function App() {
   const [Joined, setJoined] = useState(false);
